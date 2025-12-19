fc134f90be2914bef2a894f29a44e12459d4166e
blackbird8642 <18jboudrie.stem@gmail.com>
2025-12-18 19:52:30 -0500

    Added a clock and limited fps to 60

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ from logger import log_state
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     while True:
+        log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        log_state()
+            
         screen.fill(color="black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
